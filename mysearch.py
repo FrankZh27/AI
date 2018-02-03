5e6d4b9a6ab0e31fd6dee1907ad43bbf6a86e832
Frank Zhong <jzho@umich.edu>
2018-02-02 23:56:16 -0500

    Add files via upload

--- mysearch.py
+++ mysearch.py
@@ -9,10 +9,19 @@ Created on Fri Feb  2 10:36:20 2018
 class Queue:
     size = 0
     queue = list()
+    
     def insert(self, node):
         self.queue.append(node)
+        self.size = self.size + 1
+        return
         
-    
+    def dequeue(self):
+        self.queue.pop(0)
+        self.size = self.size - 1
+        return
+        
+    def getSize(self):
+        return self.size;
 
 filename = 'route.txt'
 # filename = 'tsp.txt'
